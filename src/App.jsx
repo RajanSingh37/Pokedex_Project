462748fabe24b1636263916a1899890599e0ebdd
Rajan Singh <ssrajan841@gmail.com>
2024-02-12 11:44:32 +0530

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css'
-import Pokedex from './components/Pokedex/Pokedex';
+// import Pokedex from './components/Pokedex/Pokedex';
+import CustomRoutes from './routes/CustomRoutes';
+import { Link } from 'react-router-dom';
 
 function App() {
 
   return (
-    <>
-     < Pokedex />
-    </>
+    <div className="outer-pokedex">
+    <h1 id= "pokedex-heading">
+      <Link to="/">Pokedex</Link>
+      </h1>
+     < CustomRoutes />
+    </div>
   )
 }
 
-export default App
+export default App;
--- src/components/Pokemon/Pokemon.jsx
+++ src/components/Pokemon/Pokemon.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 import './Pokemon.css';
+// import React from 'react';
 
 function Pokemon(props) {
     return (
         <div className="pokemon">
-            <div className='pokemon-name'>Name: {props.name}</div>
-            <img className='pokemon-image' src={props.image} alt={props.name} />
+            <Link to={`/pokemon/${props.id}`}>
+                <div>
+                    <div className='pokemon-name'>Name: {props.name}</div>
+                    <div>
+                        <img className='pokemon-image' src={props.image} alt={props.name} />
+                    </div>
+                </div>
+            </Link>
         </div>
     );
 }
